c8ead73b77ebd8e68caa67178f49ab6a3d7e8d27
Xorgon <norman.reggox@gmail.com>
2016-02-22 23:25:31 +0000

    Continued work

--- aclab4.py
+++ aclab4.py
@@ -1,2 +1,13 @@
+from scipy import optimize as opt
+import aclab3 as lab3
+
+
+xfoil_path = "C:/Users/Elijah/Documents/XFoil/"
+
+
 def one_dim_opt(x0, cl, file_path, xfoil_path):
-    return
+    bs = [0.6, 0.9, 1.2]
+    opt_out = opt.minimize_scalar(lab3.run_xfoil_wcl,
+                                  args=(cl, file_path, xfoil_path),
+                                  method="brent", bracket=bs)
+    return opt_out
